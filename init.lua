a4cbbab2acd8dd7152f03b530946ff05e50fafeb
Ilai Van Der Veldt <ivan-der@f0r2s12.codam.nl>
2026-02-09 13:34:36 +0100

    fixed fake tabs

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 require("config.lazy")
 vim.o.number = true
-vim.o.tabstop = 4
+-- vim.o.tabstop = 4
 vim.o.mouse = v
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+-- vim.o.softtabstop = 4
+-- vim.o.shiftwidth = 4
 vim.o.autoindent = true
 vim.o.cursorline = true
 vim.o.ttyfast = true
 vim.o.cursorline = true
 vim.o.incsearch = true
-vim.o.numberwidth = 4
+-- vim.o.numberwidth = 4
 vim.o.relativenumber = true
-vim.o.smartindent = true
+-- vim.o.smartindent = true
 vim.o.colorcolumn = ""
 vim.o.syntax = "on"
